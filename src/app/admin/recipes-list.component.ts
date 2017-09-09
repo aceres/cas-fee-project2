00c59b1d49a17b81da20607ae48345a0d98c2f1e
André Ceres <andre.ceres@gmail.com>
2017-09-09 23:07:21 +0200

    - Cleaned up

--- src/app/admin/recipes-list.component.ts
+++ src/app/admin/recipes-list.component.ts
@@ -12,15 +12,7 @@ import { RecipeService } from '../services/recipe.service';
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[];
-  selectedRecipe: Recipe;
-
   allRecipes: FirebaseListObservable<any[]>;
-
-  // @Input()
-  // recipe:  Recipe;
-
-  // @Output()
-  // deleteReceiptEvent = new EventEmitter<string>();
 
   constructor(private router: Router, private recipeService: RecipeService, db: AngularFireDatabase) {
     this.allRecipes = db.list('/recipes');
@@ -30,28 +22,6 @@ export class RecipesListComponent implements OnInit {
     // this.recipeService.getRecipes().then(recipes => this.allRecipes = recipes);
     this.recipeService.getRecipes().then(recipes => this.allRecipes);
   }
-
-  add(name: string, description: string, portion: string, prepTime: number, level: string, category: string, cuisine: string, step1: string, step2: string, step3: string, quantity: number, unit: string, ingredient: string): void {
-    name = name.trim();
-    description = description.trim();
-    portion = portion.trim();
-    level = level.trim();
-    category = category.trim();
-    cuisine = cuisine.trim();
-    step1 = step1.trim();
-    step2 = step2.trim();
-    step3 = step3.trim();
-    //if (!name && !description) { return; }
-    this.recipeService.create(name, description, portion, prepTime, level, category, cuisine, step1, step2, step3, quantity, unit, ingredient)
-      .then(recipe => {
-        // this.recipes.push(recipe);
-        // this.selectedRecipe = null;
-      });
-  }
-
-  // remove() {
-  //   this.deleteReceiptEvent.emit(this.recipe.$key)
-  // }
 
   remove(id): void {
     this.recipeService
@@ -66,13 +36,7 @@ export class RecipesListComponent implements OnInit {
     this.getRecipes();
   }
 
-  onSelect(recipe: Recipe): void {
-    this.selectedRecipe = recipe;
-  }
-
   gotoDetail(recipe): void {
-    console.log('Recipe: ', recipe);
-    console.log('Recipe Key: ', recipe.$key);
     this.router.navigate(['/admin/recipe-detail', recipe.$key]);
   }
 }
