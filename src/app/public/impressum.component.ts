a1059e1d887f63601ba4b33f05c216fe25abee84
Tanja Sennhauser <tanja.sennhauser@nobel-web-engineering.ch>
2017-10-24 20:26:52 +0200

    renamed impressum component

--- src/app/public/impressum.component.ts
+++ src/app/public/impressum.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-impressum',
-  templateUrl: './public.impressum.html',
-  styleUrls: ['./public.impressum.less']
+  templateUrl: './impressum.component.html',
+  styleUrls: ['./impressum.component.less']
 })
 export class ImpressumComponent {
   title = 'Impressum';
