35a037e174e4baeda5cde133efd7d3b1ac959629
André Ceres <andre.ceres@gmail.com>
2017-10-31 19:41:57 +0100

    - refactored the detail page - admin

--- src/app/admin/view-detail/recipe-detail.component.ts
+++ src/app/admin/view-detail/recipe-detail.component.ts
@@ -1,8 +1,8 @@
 import 'rxjs/add/operator/switchMap';
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
-import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 import { Recipe } from '../../services/recipe';
 import { RecipeService } from '../../services/recipe.service';
@@ -12,21 +12,18 @@ import { RecipeService } from '../../services/recipe.service';
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe;
-  key;
+  recipe: Recipe;
 
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
     private location: Location
-  ) { }
+  ) {}
 
-  ngOnInit() {
-    this.route.params.forEach((urlParameters) => {
-      const id = urlParameters['id'];
-      this.recipe = this.recipeService.getRecipe(id);
-    });
-    this.key = this.route.snapshot.params['id'];
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.recipeService.getRecipe(params.get('id')))
+      .subscribe(recipe => this.recipe = recipe);
   }
 
   goBack(): void {
